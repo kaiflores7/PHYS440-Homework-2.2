c74a14a519b5dcab218735f2f68034639a91f4d6
kaiflores7 <kflores7@hawk.iit.edu>
2022-02-04 12:54:06 -0600

    Spa Bessel

---
+++ Shared/Bessel Function.swift
@@ -0,0 +1,109 @@
+//
+//  Bessel Function.swift
+//  Homework 2.2
+//
+//  Created by Katia Flores on 2/4/22.
+//
+
+import Foundation
+import SwiftUI
+
+
+/* Calculate Bessel functions using downward recursion */
+/// calculateDownwardRecursion
+/// - Parameters:
+///   - xValue: x
+///   - order: Order of Bessel Function
+///   - start: Starting Order
+///              2l + 1
+///     J       (x)  =   ------ J  (x)   -  J        (x)
+///      l - 1              x       l             l + 1
+///
+///
+func calculateDownwardRecursion (xValue: Double, order: Int, start: Int) -> Double
+{
+    var scale = calculateFirstBessel(xValue: xValue) /* jSub0 which we know. Used for scaling the downward recursion */
+    
+    var jSubl = Array(repeating: 0.0, count: start + 2) /* jSubl is an array that holds the various orders of the Bessel Function */
+    
+    
+    jSubl[start+1] = 1.0                   // start with "guess"
+    jSubl[start] = 1.0                      // start with "guess"
+    
+    for index in (1...start).reversed(){
+        
+        jSubl[index-1] = ((2.0*(Double(index))+1.0)/xValue)*jSubl[index] - jSubl[index+1]
+    }
+    
+    scale = (scale)/jSubl[0];      /* scale the result */
+    
+    return(jSubl[order]*(scale))
+}
+
+
+/* Calculate Bessel functions using upward recursion */
+/// calculateUpwardRecursion
+/// - Parameters:
+///   - xValue: x
+///   - order: Order of Bessel Function
+///               2l + 1
+///     J          (x)  =   ------  J  (x)  -  J       (x)
+///       l + 1              x        l            l - 1
+///
+///
+func calculateUpwardRecursion (xValue: Double, order: Int) -> Double
+{
+    var firstBessel = 0.0  /* temporary placeholders through the upward recursion */
+    var secondBessel = 0.0 /* temporary placeholders through the upward recursion */
+    var thirdBessel = 0.0; /* holds final Bessel Function result */
+    
+    
+    firstBessel = calculateFirstBessel(xValue: xValue)                    /* start with lowest order */
+    if (order == 0) {
+        thirdBessel = firstBessel
+    }
+    else {
+        secondBessel = calculateSecondBessel(xValue: xValue)
+        for index in (1..<order)             /* loop for order of function */
+        {
+            thirdBessel = ((2.0*(Double(index))+1.0)/xValue)*secondBessel - firstBessel       // recursion relation
+            firstBessel = secondBessel
+            secondBessel = thirdBessel
+        }
+    }
+    return(thirdBessel)
+}
+
+
+
+/// calculateFirstBessel
+/// - Parameter xValue: x
+/// - Returns: first Bessel Function
+
+func calculateFirstBessel (xValue: Double) -> Double{
+    
+    var j = 0.0
+    
+    j = sin(xValue) / xValue
+    
+    return j
+    
+}
+
+
+/// calculateSecondBessel
+/// - Parameter xValue: x
+/// - Returns: second Bessel Function
+
+func calculateSecondBessel (xValue: Double) -> Double{
+    
+    var j = 0.0
+    let sinFunc = sin(xValue) / (xValue↑(2.0))
+    let cosFunc = cos(xValue) / xValue
+    
+    j = sinFunc - cosFunc
+    
+    return j
+    
+}
+
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,10 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var guess = ""
+    @State private var totalInput: Int? = 18
+    
+    private var intFormatter: NumberFormatter = {
+        let f = NumberFormatter()
+        f.numberStyle = .decimal
+        return f
+    }()
+    
     var body: some View {
-        Text("Hello, world!")
+    
+       VStack {
+            HStack {
+                
+                TextEditor(text: $guess)
+
+                
+                Button("Calculate Spherical Bessel Functions", action: calculateBesselFunc)
+                    }
+            .frame(minHeight: 300, maxHeight: 800)
+            .frame(minWidth: 480, maxWidth: 800)
             .padding()
+        
+        HStack{
+            
+            Text(verbatim: "Order:")
+            .padding()
+            TextField("Total", value: $totalInput, formatter: intFormatter)
+        
+                .padding()
+            
+            }
+        }
+        
     }
+    
+    func calculateBesselFunc()  {
+
+    let xmax = 16.0                     /* max of x  */
+    let xmin = 0.1                     /* min of x >0  */
+    let step = 0.1                      /* delta x  */
+    let order = totalInput ?? 0                      /* order of Bessel function */
+    let start = order+25                      /* used for downward algorithm */
+    var x = 0.0
+    var maxIndex = 0
+    guess = String(format: "J%d(x)\n", order)
+    maxIndex = Int(((xmax-xmin)/step))+1
+
+    for index in (0...maxIndex)
+    {
+        x = Double(index)*step + xmin
+        
+        guess += String(format: "x = %f, Downward, %7.5e, Upward, %7.5e\n", x, calculateDownwardRecursion(xValue: x, order: order, start: start),   calculateUpwardRecursion(xValue: x, order: order))
+        
+    }
+        
+
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
